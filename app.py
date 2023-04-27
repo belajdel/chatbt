bd4c3472c0a0a064a0815ae59a211cce2dc15656
AGS <107185801+AGS2023@users.noreply.github.com>
2023-04-27 22:09:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask,request,render_template,jsonify
 from chat import get_response
+from flask_cors import CORS
 app=Flask(__name__)
+CORS(app)
 @app.get("/")
 def index_get():
  return render_template("base.html")
